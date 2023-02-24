0537bda60d1aa7b63d3651c1f1740ed90a95dbe5
Ali Başoğlu <alibasoglu1911@gmail.com>
2023-02-24 15:15:56 +0300

    🚧 Implement header interceptor

--- app/src/main/java/com/alibasoglu/cinemax/di/AppModule.kt
+++ app/src/main/java/com/alibasoglu/cinemax/di/AppModule.kt
@@ -1,12 +1,36 @@
 package com.alibasoglu.cinemax.di
 
+import com.alibasoglu.cinemax.BuildConfig
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val headerApiKey = BuildConfig.MOVIES_HEADER_API_KEY
+        val headerInterceptor = Interceptor { chain ->
+            chain.run {
+                proceed(
+                    request()
+                        .newBuilder()
+                        .addHeader("Authorization", "Bearer $headerApiKey")
+                        .addHeader("Content-Type", "application/json;charset=utf-8")
+                        .build()
+                )
+            }
+        }
 
+        return OkHttpClient.Builder()
+            .addInterceptor(headerInterceptor)
+            .build()
+    }
 }
