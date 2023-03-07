1a9285bacb43ec1615e5e57c255e3954932adfdb
Ali Başoğlu <alibasoglu1911@gmail.com>
2023-03-07 11:03:26 +0300

    ✨Implement one time OnBoarding Fragment

--- app/src/main/java/com/alibasoglu/cinemax/MainViewModel.kt
+++ app/src/main/java/com/alibasoglu/cinemax/MainViewModel.kt
@@ -1,21 +1,28 @@
 package com.alibasoglu.cinemax
 
+import android.content.SharedPreferences
 import androidx.lifecycle.viewModelScope
 import com.alibasoglu.cinemax.core.BaseViewModel
 import com.alibasoglu.cinemax.domain.usecase.SetImagesConfigDataUseCase
+import com.alibasoglu.cinemax.ui.onboarding.OnboardingViewModel.Companion.ONBOARDING_KEY
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.launch
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val setImagesConfigDataUseCase: SetImagesConfigDataUseCase
+    private val setImagesConfigDataUseCase: SetImagesConfigDataUseCase,
+    private val sharedPreferences: SharedPreferences
 ) : BaseViewModel() {
 
     fun getSetImagesConfigData() {
         viewModelScope.launch {
             setImagesConfigDataUseCase()
         }
+    }
+
+    fun getShouldShowOnBoarding(): Boolean {
+        return sharedPreferences.getBoolean(ONBOARDING_KEY, true)
     }
 
 }
--- app/src/main/java/com/alibasoglu/cinemax/ui/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/alibasoglu/cinemax/ui/onboarding/OnboardingFragment.kt
@@ -2,18 +2,24 @@ package com.alibasoglu.cinemax.ui.onboarding
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
 import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import com.alibasoglu.cinemax.R
 import com.alibasoglu.cinemax.core.fragment.BaseFragment
 import com.alibasoglu.cinemax.core.fragment.FragmentConfiguration
 import com.alibasoglu.cinemax.databinding.FragmentOnboardingBinding
+import com.alibasoglu.cinemax.utils.setImageDrawableWithAnimation
 import com.alibasoglu.cinemax.utils.viewbinding.viewBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class OnboardingFragment : BaseFragment(R.layout.fragment_onboarding) {
     override val fragmentConfiguration = FragmentConfiguration()
 
     private val binding by viewBinding(FragmentOnboardingBinding::bind)
 
+    private val viewModel by viewModels<OnboardingViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -31,23 +37,45 @@ class OnboardingFragment : BaseFragment(R.layout.fragment_onboarding) {
                 override fun onPageSelected(position: Int) {
                     when (position) {
                         0 -> {
-                            progressButton.setImageDrawable(resources.getDrawable(R.drawable.progress_button_1))
+                            progressButton.setImageDrawableWithAnimation(
+                                ContextCompat.getDrawable(
+                                    requireContext(),
+                                    R.drawable.progress_button_1
+                                ), 200
+                            )
                         }
                         1 -> {
-                            progressButton.setImageDrawable(resources.getDrawable(R.drawable.progress_button_2))
+                            progressButton.setImageDrawableWithAnimation(
+                                ContextCompat.getDrawable(
+                                    requireContext(),
+                                    R.drawable.progress_button_2
+                                ), 200
+                            )
                         }
                         2 -> {
-                            progressButton.setImageDrawable(resources.getDrawable(R.drawable.progress_button_3))
+                            progressButton.setImageDrawableWithAnimation(
+                                ContextCompat.getDrawable(
+                                    requireContext(),
+                                    R.drawable.progress_button_3
+                                ), 200
+                            )
                         }
                     }
                 }
+
                 override fun onPageScrollStateChanged(state: Int) {}
             })
             progressButton.setOnClickListener {
-                //TODO scroll to next one
+                when (viewPager.currentItem) {
+                    0 -> viewPager.currentItem = 1
+                    1 -> viewPager.currentItem = 2
+                    2 -> {
+                        viewModel.setOnBoardingShown()
+                        nav(OnboardingFragmentDirections.actionOnboardingFragmentToIntroFragment())
+                    }
+                }
             }
         }
-
 
     }
 
