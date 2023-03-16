62ba42b3e456e07d6b2f75c938a392b1e3ff99c8
Ali Başoğlu <alibasoglu1911@gmail.com>
2023-03-16 10:27:02 +0300

    🐛Bugfix caused by blur effect under API 31

--- app/src/main/java/com/alibasoglu/cinemax/utils/BlurEffect.kt
+++ app/src/main/java/com/alibasoglu/cinemax/utils/BlurEffect.kt
@@ -4,16 +4,18 @@ import android.app.Activity
 import android.graphics.RenderEffect
 import android.graphics.Shader
 import android.os.Build
-import androidx.annotation.RequiresApi
 
-@RequiresApi(Build.VERSION_CODES.S)
+
 fun showBlurEffect(activity: Activity) {
-    activity.window.decorView.setRenderEffect(
-        RenderEffect.createBlurEffect(15f, 15f, Shader.TileMode.CLAMP)
-    )
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        activity.window.decorView.setRenderEffect(
+            RenderEffect.createBlurEffect(15f, 15f, Shader.TileMode.CLAMP)
+        )
+    }
 }
 
-@RequiresApi(Build.VERSION_CODES.S)
 fun clearBlurEffect(activity: Activity) {
-    activity.window.decorView.setRenderEffect(null)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        activity.window.decorView.setRenderEffect(null)
+    }
 }
